3b9e3b736e796cfe13f5b681ab97ff7f31af487a
truenotzero <truenotzero@gmail.com>
2023-12-12 23:22:48 +0200

    solved: day 03 part 1

--- day03/src/bin/part1.rs
+++ day03/src/bin/part1.rs
@@ -1,121 +1,94 @@
-use std::fs;
+use std::collections::HashSet;
 
-use itertools::Itertools;
 
 fn main() {
     do_file("./input.txt");
 }
 
-#[derive(Debug)]
-struct Array {
-    vec: Vec<Vec<char>>,
-    width: isize,
-    height: isize,
-}
+type Num = i32;
 
-impl Array {
-    fn new(scheme: &str) -> Self {
-        let width= scheme.lines().next().unwrap().len(); // x
-        let height = scheme.lines().count(); // y
-        let mut vec = vec![vec!['\0'; width]; height];
-
-        for (y, line) in scheme.lines().enumerate() {
-            for (x, ch) in line.chars().enumerate() {
-                vec[y][x] = ch;
-            }
-        }
-
-        Self {
-            vec,
-            width: width as isize,
-            height: height as isize,
-        }
-    }
-
-    fn get(&self, x: isize, y: isize) -> Option<char> {
-        self.vec.get(y as usize).and_then(|e| e.get(x as usize)).copied()
-    }
-    
-    fn cardinals(&self, x: isize, y: isize) -> Vec<(isize, isize)> {
-        let mut ret = Vec::new();
-        for dx in -1..=1 {
-            for dy in -1..=1 {
-                if dx == 0 && dy == 0 { continue }
-                if let Some(_) = self.get(x+dx, y+dy) {
-                    ret.push((x+dx, y+dy));
-                }
-            }
-        }
-        // println!("cardinals: ({x},{y}) => {ret:?}");
-        ret
-    }
-
-    fn parse_number(&self, x: isize, y: isize) -> Option<u32> {
-        // first check that this is even a digit string
-        self.get(x, y)?.to_digit(10)?;
-        // find first digit in string
-        let start_idx = {
-            // we can check the one before automatically because
-            // it's a given that the current x is a digit
-            let mut x = x - 1;
-            loop {
-                // if we reached a . then the first digit is the x after this x
-                if self.get(x, y)? == '.' { break x + 1 }
-                // if we hit the start of the line then return anyway
-                if x == 0 { break x } // 
-                x -= 1;
-            }
-        };
-
-        let mut acc = 0;
-        for x in start_idx..self.width {
-            let ch = self.get(x, y);
-            // println!("parse: ({x},{y})=>{ch:?}");
-            match ch.and_then(|ch| ch.to_digit(10)) {
-                Some(n) => acc = 10 * acc + n,
-                None => break,
-            }
-        }
-
-        Some(acc)
-    }
-}
-
-fn process(scheme: &str) -> u32 {
-    let arr = Array::new(scheme);
-    (0..arr.height)
-        .flat_map(|y| {
-            (0..arr.width).map(move |x| (x, y))
-        })
-        .filter(|(x,y)| { // filter to symbol coords
-            let ch = arr.get(*x, *y).unwrap_or('.');
-            // check not dot
-            if ch == '.' { return false }
-            // check not digit
-            if ch.is_ascii_digit() { return false }
-            
-            true
-        })
-        .flat_map(|(x,y)| arr.cardinals(x, y)) // get each symbol's cardinals
-        .unique() // remove duplicate coords
-        .filter_map(|(x,y)| arr.parse_number(x, y)) // parse each coord into a number or filter it if not a number
-        .unique() // remove duplicate numbers
-        .sum()
-}
-
-fn do_file(name: &str) -> u32 {
-    let input = fs::read_to_string(name).unwrap();
+fn do_file(filename: &str) -> Num {
+    let input = std::fs::read_to_string(filename).unwrap();
     let output = process(&input);
     println!("{output}");
     output
 }
 
+type Schematic = Vec<Vec<char>>;
+type Coords = (Num, Num); // yx
+
+fn find_symbols(schematic: &Schematic) -> Vec<Coords> {
+    let mut ret = Vec::new();
+    for (y, row) in schematic.iter().enumerate() {
+        for (x, e) in row.iter().enumerate() {
+            if '.'.ne(e) && !e.is_digit(10) {
+                ret.push((y as _, x as _))
+            }
+        }
+    }
+    ret
+}
+
+fn adjacents(yx: Coords, bounds: Coords) -> Vec<Coords> {
+    let mut ret = Vec::new();
+    let offsets = -1..=1;
+    for dy in offsets.clone() {
+        let y = yx.0 + dy;
+        if y < 0 || y >= bounds.0 { continue }
+        for dx in offsets.clone() {
+            if dx == 0 && dy == 0 { continue }
+            let x = yx.1 + dx;
+            if x < 0 || x >= bounds.1 { continue }
+            ret.push((y, x));
+        }
+    }
+    ret
+}
+
+fn try_parse_num(yx: Coords, schematic: &Schematic) -> Option<(Num, Coords)> {
+    if !schematic[yx.0 as usize][yx.1 as usize].is_digit(10) { return None }
+    let start = {
+        let mut x = yx.1;
+        let y = yx.0;
+        loop {
+            // break before non-digit idx
+            let nx = x - 1;
+            if nx < 0 { break }
+            let next = schematic[y as usize][nx as usize];
+            if !next.is_digit(10) { break }
+            x = nx;
+        }
+        x
+    };
+
+    let mut digits = Vec::new();
+    for e in schematic[yx.0 as usize][(start as usize)..].iter() {
+        if let Some(digit) = e.to_digit(10) {
+            digits.push(digit as _);
+        } else {
+            break;
+        }
+    }
+    
+    let ret = digits.iter().fold(0, |acc, e| 10 * acc + e);
+    Some((ret, (yx.0, start)))
+}
+
+fn process(input: &str) -> Num {
+    let schematic = input.lines().map(|e| e.chars().collect()).collect();
+    find_symbols(&schematic).into_iter()
+        .flat_map(|symbol| adjacents(symbol, (schematic.len() as _, schematic[0].len() as _)))
+        .filter_map(|adj| try_parse_num(adj, &schematic))
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .map(|(num, _yx)| num)
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::do_file;
-
     #[test]
     fn example() {
-        assert_eq!(do_file("./example.txt"), 4361);
+        assert_eq!(crate::do_file("./example.txt"), 4361);
     }
 }
